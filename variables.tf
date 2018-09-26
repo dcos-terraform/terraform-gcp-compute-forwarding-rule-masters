27635dd466eaf43ea6dd62df2722ad90b6e53a24
Jan Ulferts <julferts@mesosphere.com>
2018-09-26 15:13:38 +0200

    we have to shorten load balancer names

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "name_prefix" {
 
 variable "name_format" {
   description = "printf style format for naming the ELB. Gets truncated to 32 characters. (input cluster_name)"
-  default     = "%s-master-forwarding-rule"
+  default     = "m-%s"
 }
 
 variable "masters_self_link" {
